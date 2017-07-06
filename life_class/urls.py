842903e740173590e2484651007d245c0d76e3d1
shs2810 <shs28100@naver.com>
2017-07-06 16:30:09 +0900

    gitignore 추가, life_class.html 수정

--- life_class/urls.py
+++ life_class/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.life_class, name ='life_class'),
-    url(r'^life_class_detail/$', views.life_class_detail, name='life_class_detail'),
+    url(r'^life_class_detail/(?P<pk>[0-9]+)/$', views.life_class_detail, name='life_class_detail'),
+    # ^ - 시작
+    # (?P<pk>[0-9]+) 장고가 pk 변수에 모든 값을 넣어 뷰로 전송한다 [0-9] 은 문자를 제외한 0~9중 한 가지 숫자만 온다는 뜻. + 는 그 이상의 숫자가 올 수 있다는 뜻
+    # $ - 마지막, 그 뒤로 문자가 더 오면 안된다.
+    # ex) 127.0.0.1:8000/life_class_detail/5/ 이면 life_calss_detail 뷰를 찾아 매개변수 pk가 5인 값을 찾아 뷰로 전달
     url(r'^life_class_write/$', views.life_class_write, name='life_class_write'),
 ]
