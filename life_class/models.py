2ac953e65a27a4b608ba6203bc4f4763c86e68b0
dennyoh <dennyoh9211@gmail.com>
2017-07-07 15:57:03 +0900

    fixed untracked files

--- life_class/models.py
+++ life_class/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class LifeClass(models.Model):
     month = models.IntegerField()
     year = models.IntegerField()
     sex = models.CharField(max_length=10)
-    title = models.CharField(max_length=200)
     contents = models.TextField()
 
     def __str__(self):
