55b85ac37c5853547d43df92d29a3c1f347d9610
dennyoh <dennyoh9211@gmail.com>
2017-07-07 16:26:17 +0900

    commit

--- life/settings.py
+++ life/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'life.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'Name'
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
