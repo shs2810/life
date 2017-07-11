38d51e75189db70c237c1da089a8b72c7e8092c5
shs2810 <shs28100@naver.com>
2017-07-11 17:02:39 +0900

    모델 시간 추가 / 댓글 url, view 추가

--- life_class/models.py
+++ life_class/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 import datetime
 
 # Create your models here.
@@ -8,6 +10,12 @@ class LifeClass(models.Model):
     sex = models.CharField(max_length=3)
     contents = models.TextField()
     tags = models.CharField(max_length=30)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.tags
+
+class Comment(models.Model):
+    pass
+   # lifeclass = models.ForeignKey(LifeClass) #LifeClass 글이랑 댓글 1:1 관계
+
