e4c30ed67dcc403859ad03ac9c98f7cf831239b0
shs2810 <shs28100@naver.com>
2017-07-13 19:29:30 +0900

    D

--- life_class/models.py
+++ life_class/models.py
@@ -16,16 +16,7 @@ class LifeClass(models.Model):
         return self.tags
 
 class Comment(models.Model):
-    lifeclass = models.ForeignKey('life_class.LifeClass', related_name='comments')
     message = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    approved_comment = models.BooleanField(default=False)
-
-    def approve(self):
-        self.approved_comment = True
-        self.save()
-
-    def __str__(self):
-        return self.message
+    created_at = models.DateTimeField(auto_now_add=True)
    # lifeclass = models.ForeignKey(LifeClass) #LifeClass 글이랑 댓글 1:1 관계
 
