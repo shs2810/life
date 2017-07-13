6f4aea22d266e0c3b6c242858c19ac24506e1a93
dennyoh <dennyoh9211@gmail.com>
2017-07-13 19:34:14 +0900

    ignore

--- life_class/views.py
+++ life_class/views.py
@@ -1,4 +1,9 @@
+import requests
+
 from django.shortcuts import render, get_object_or_404, redirect
+from django.conf import settings
+from django.contrib import messages
+
 from .models import *
 from .forms import *
 
@@ -18,25 +23,31 @@ def life_class_detail(request, pk):
 
 def life_class_comment_add(request):
     pass
-
 #life_class_write
 
 def life_class_write(request):
-    form = LifeClassForm
-    return render(request, 'life_class/life_class_write.html')
-
-<<<<<<< HEAD
-def life_class_comment_add(request):
-    pass
-=======
-def create(request):
-    if request.methos=='POST':
+    if request.method=='POST':
         form = LifeClassForm(request.POST)
-        if form.is_calid():
-            form.save()
-        return redirect('/lifeclass/list')
+        if form.is_valid():
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            data = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+            result = r.json()
+            if result['success']:
+                lifeclass = form.save()
+                lifeclass.created_at = timezone.now()
+                lifeclass.save()
+            else:
+                messages.error(request, '체크 해주세요')
+                return render(request, 'life_class/life_class_write.html', {'form': form})
+        return redirect('life_class_detail', pk=lifeclass.pk)
     else:
         form = LifeClassForm()
 
     return render(request, 'life_class/life_class_write.html', { 'form':form })
->>>>>>> 6f27cb2962a8a8d0b8180cd5d050f94bf9569de9
+
+
+
